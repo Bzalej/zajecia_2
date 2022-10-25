f341686077e4fad6d46c9b1e7c9cefec48ab1f10
blazej <wituckib@gmail.com>
2022-10-25 20:27:28 +0200

    while tablicy kotow

--- src/Foreach.java
+++ src/Foreach.java
@@ -1,6 +1,6 @@
 public class Foreach {
     public static void main(String[] args) {
-        String [] catTable=new String[]{"Kazia","Boczek","Sernik","Julian","Juliusz","Mariusz","Wariat","Batman","Geriatryk"};
+        String [] catTable=new String[]{"Kazia","Boczek","Sernik","Julian","Juliusz","Mariusz","Wariat","Geriatryk","Batman"};
         for (String cat:catTable){
             System.out.println(cat);
         }
